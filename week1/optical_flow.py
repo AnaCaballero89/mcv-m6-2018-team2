9e9fe12154a6da13355fd2955ffba50b25b9349d
Guillem Delgado Gonzalo <guillem.delgado@gmail.com>
2018-02-27 03:18:18 +0100

    Fixed errors on task 3
    Still has a problem which doesnt calculate correctly the histogram nor the MSEN & PEPN

--- week1/optical_flow.py
+++ week1/optical_flow.py
@@ -1,14 +1,8 @@
 import cv2
 import numpy as np
-import glob
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import precision_recall_fscore_support as score
-import json
 import math
 import matplotlib
-from numpy.random import randn
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
 
 
 def msen(opticalflowResults, opticalflowGT):
@@ -16,23 +10,70 @@ def msen(opticalflowResults, opticalflowGT):
     # opticalflowGT: Results of the optical flow from the ground truth.
 
     error_count = 0
-    results = cv2.imread(opticalflowResults).flatten()
-    opticalflow = cv2.imread(opticalflowGT).flatten()
+    error = []
 
-    error = [gt - pred for gt, pred in zip(opticalflow, results)]
-    mse = [x**2 for x in error]
-    total_mse = (1/results.shape[0]) * sum(mse)
+    results = cv2.imread(opticalflowResults, -1)
+    opticalflow = cv2.imread(opticalflowGT, -1)
+
+    # flow_u(u, v) = ((float)I(u, v, 1) - 2 ^ 15) / 64.0;
+    # flow_v(u, v) = ((float) I(u, v, 2) - 2 ^ 15) / 64.0;
+    # valid(u, v) = (bool)I(u, v, 3);
+
+    results_u1 = results[:, :, 1].flatten().astype(np.float)
+    results_v1 = results[:, :, 2].flatten().astype(np.float)
+    results_validation = results[:, :, 0].flatten()
+
+    results_u = [(pred - math.pow(2, 15)) / 64.0 for pred in results_u1]
+    results_v = [(pred - math.pow(2, 15)) / 64.0 for pred in results_v1]
+
+    for i in range(len(results_validation)):
+        results_u[i] = results_u[i] * results_validation[i]
+        results_v[i] = results_v[i] * results_validation[i]
+
+
+
+
+    groundtruth_u1 = opticalflow[:, :, 1].flatten().astype(np.float)
+    groundtruth_v1 = opticalflow[:, :, 2].flatten().astype(np.float)
+    groundtruth_validation = opticalflow[:, :, 0].flatten()
+
+    groundtruth_u = [(gt - math.pow(2, 15)) / 64.0 for gt in groundtruth_u1]
+    groundtruth_v = [(gt - math.pow(2, 15)) / 64.0 for gt in groundtruth_v1]
+
+    for i in range(len(groundtruth_validation)):
+        groundtruth_u[i] = groundtruth_u[i] * groundtruth_validation[i]
+        groundtruth_v[i] = groundtruth_v[i] * groundtruth_validation[i]
+
+
+    #error = [gt - pred for gt, pred in zip(opticalflow, results)]
+
+    error = np.zeros(len(groundtruth_u))
+    for i in range(len(groundtruth_u)):
+        error[i] = math.sqrt(math.pow((groundtruth_u[i] - results_u[i]), 2) + math.pow((groundtruth_v[i] - results_v[i]), 2))
+
+
+    #mse = [x**2 for x in error]
+    total_mse = (1/len(error)) * sum(error)
 
     print('Mean square error in Non-Occluded areas: {} '.format(total_mse))
 
     for e in error:
-        if abs(e) > 3:
+        if e > 3:
             error_count += 1
 
-    pepn = 100 * (error_count / results.shape[0])
+    pepn = 100 * (error_count / sum(groundtruth_validation))
     print('Percentage of Erroneous Pixels in Non-Occluded areas: {} % '.format(pepn))
 
-def representation_OF():
+    representation_OF(abs(error))
+
+def representation_OF(error):
+    num_bins = 100
+    n, bins, patches = plt.hist(error, num_bins, normed=1, facecolor='blue')
+    plt.xlabel('Mean Square Error in Non-Occluded areas')
+    plt.ylabel('Percentage of errors')
+    plt.title(r'Histogram of errors')
+    plt.show()
+
 
 
 if __name__ == "__main__":
