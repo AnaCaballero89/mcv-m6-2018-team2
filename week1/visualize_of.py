3312f6af3615e012fb18896b152b0259e7ef2ad0
franroldans <fran.roldans@gmail.com>
2018-02-28 10:13:35 +0100

    Add files via upload

--- week1/visualize_of.py
+++ week1/visualize_of.py
@@ -2,30 +2,30 @@ import cv2
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import matplotlib.axes as mplax
+from skimage.measure import block_reduce
+from skimage.transform import resize
 
+results1= './results/kitti/LKflow_000045_10.png'
+results = cv2.imread(results1, -1)
+image = './results/kitti/000045_10.png'
+image = cv2.imread(image, -1)
 
 u = []
 v = []
-results1= './results/kitti/LKflow_000045_10.png'
-image = './results/kitti/000045_10.png'
-results = cv2.imread(results1, -1)
+
+results = block_reduce(results, block_size=(8, 8, 1), func=np.mean)
 c1, c2, c3 = results.shape
-print c1
-print c2
-print c3
-x, y = np.meshgrid(np.arange(0, c1, 1), np.arange(0, c2, 1))
-#x, y = np.meshgrid(np.arange(0, 2 * np.pi, 1), np.arange(0, 2 * np.pi, 1))
-results_u1 = results[:, :, 1].flatten().astype(np.float)
-results_v1 = results[:, :, 2].flatten().astype(np.float)
-results_validation = results[:, :, 0].flatten()
-results_u = [((pred - math.pow(2, 15)) / 64.0)/200.0 for pred in results_u1]
-results_v = [((pred - math.pow(2, 15)) / 64.0)/200.0 for pred in results_v1]
-for i in range(len(results_validation)):
-    results_u[i] = results_u[i] * results_validation[i]
-    results_v[i] = results_v[i] * results_validation[i]
+
+x, y = np.meshgrid(np.arange(0, c2, 1), np.arange(0, c1, 1))
+results_u = [(((float)(results[:, :, 1].flat[pixel]) - math.pow(2, 15)) / 64.0)/200.0 if results[:, :, 0].flat[pixel] == 1 else 0
+     for pixel in range(0, results[:, :, 0].size)]
+results_v = [(((float)(results[:, :, 2].flat[pixel]) - math.pow(2, 15)) / 64.0)/200.0 if results[:, :, 0].flat[pixel] == 1 else 0
+     for pixel in range(0, results[:, :, 0].size)]
+
 u = np.reshape(results_u, (c1, c2))
 v = np.reshape(results_v, (c1, c2))
-plt.figure()
-plt.quiver(x, y, u, v, units='inches', linewidth = 0.01)
-plt.savefig('of_rep.png')
+img = resize(image, (c1, c2))
+
+plt.imshow(img)
+Q = plt.quiver(x, y, u, v, pivot='mid', units='inches', scale_units='inches')
+plt.show(Q)
