c14da58c2d8cf56bad3ea9924e0a044d25558c0c
vsegura93 <victor.seguratir@e-campus.uab.cat>
2018-03-03 19:34:05 +0100

    Included groundtruth paths and removed unnecessary imports

--- week2/task1.py
+++ week2/task1.py
@@ -4,38 +4,44 @@ __license__ = "M6 Video Analysis. Task 1"
 
 # Import libraries
 import os
-import math
-import cv2
-import numpy as np
 from train import *
 from gaussian import *
 
 # Highway sequences configuration, range 1050 - 1350
-highway_path = "datasets/highway/input/"	
+highway_path_in = "datasets/highway/input/"	
+highway_path_gt = "datasets/highway/groundtruth/"
 highway_alpha = 2.5
 
 # Fall sequences configuration, range 1460 - 1560
-fall_path = "datasets/fall/input/"  
+fall_path_in = "datasets/fall/input/"  
+fall_path_gt = "datasets/fall/groundtruth/"  
 fall_alpha = 2.5
 
 # Traffic sequences configuration, range 950 - 1050
-traffic_path = "datasets/traffic/input/"
+traffic_path_in = "datasets/traffic/input/"
+traffic_path_gt = "datasets/traffic/groundtruth/"
 traffic_alpha = 3.25
 
 
 if __name__ == "__main__":
 
-    # Adaptive modelling
+    # Gaussian modelling
     # First 50% of the test sequence to model background
     # Second 50% to segment the foreground
 
-    mean_matrix, std_matrix = training(highway_path, 1050, 1199, highway_alpha);
-    gaussian(highway_path, 1200, 1349, mean_matrix, std_matrix, highway_alpha); 
+    print "Computing gaussian modelling on highway dataset..."
+    mean_matrix, std_matrix = training(highway_path_in, 1050, 1199, highway_alpha);
+    gaussian(highway_path_in, 1200, 1349, mean_matrix, std_matrix, highway_alpha); 
+    print "Computing gaussian modelling on highway dataset... done"
 
-    mean_matrix, std_matrix = training(fall_path, 1460, 1509, fall_alpha);
-    gaussian(fall_path, 1510, 1559, mean_matrix, std_matrix, fall_alpha);   
+    print "Computing gaussian modelling on fall dataset..."
+    mean_matrix, std_matrix = training(fall_path_in, 1460, 1509, fall_alpha);
+    gaussian(fall_path_in, 1510, 1559, mean_matrix, std_matrix, fall_alpha);   
+    print "Computing gaussian modelling on fall dataset... done"
 
-    mean_matrix, std_matrix = training(traffic_path, 950, 999, traffic_alpha);
-    gaussian(traffic_path, 1000, 1049, mean_matrix, std_matrix, traffic_alpha);
-   
+    print "Computing gaussian modelling on traffic dataset..."
+    mean_matrix, std_matrix = training(traffic_path_in, 950, 999, traffic_alpha);
+    gaussian(traffic_path_in, 1000, 1049, mean_matrix, std_matrix, traffic_alpha);
+    print "Computing gaussian modelling on traffic dataset... done"
+
 
--- week2/task2.py
+++ week2/task2.py
@@ -4,24 +4,24 @@ __license__ = "M6 Video Analysis. Task 2"
 
 # Import libraries
 import os
-import math
-import cv2
-import numpy as np
 from train import *
 from adaptive import *
 
 # Highway sequences configuration, range 1050 - 1350
-highway_path = "datasets/highway/input/"	
+highway_path_in = "datasets/highway/input/"	
+highway_path_gt = "datasets/highway/groundtruth/"
 highway_alpha = 2.5
 highway_rho = 0.2
 
 # Fall sequences configuration, range 1460 - 1560
-fall_path = "datasets/fall/input/"  
+fall_path_in = "datasets/fall/input/"  
+fall_path_gt = "datasets/fall/groundtruth/"  
 fall_alpha = 2.5
 fall_rho = 0.5
 
 # Traffic sequences configuration, range 950 - 1050
-traffic_path = "datasets/traffic/input/"
+traffic_path_in = "datasets/traffic/input/"
+traffic_path_gt = "datasets/traffic/groundtruth/"
 traffic_alpha = 3.25
 traffic_rho = 0.15
 
@@ -32,13 +32,18 @@ if __name__ == "__main__":
     # First 50% frames for training
     # Second 50% left backgrounds adapts
 
-    mu_matrix, sigma_matrix = training(highway_path, 1050, 1199, highway_alpha);
-    adaptive(highway_path, 1200, 1349, mu_matrix, sigma_matrix, highway_alpha, highway_rho); 
+    print "Computing adaptive modelling on highway dataset..."
+    mu_matrix, sigma_matrix = training(highway_path_in, 1050, 1199, highway_alpha);
+    adaptive(highway_path_in, 1200, 1349, mu_matrix, sigma_matrix, highway_alpha, highway_rho); 
+    print "Computing adaptive modelling on hihighway dataset... done"
 
-    mu_matrix, sigma_matrix = training(fall_path, 1460, 1509, fall_alpha);
-    adaptive(fall_path, 1510, 1559, mu_matrix, sigma_matrix, fall_alpha, fall_rho);   
+    print "Computing adaptive modelling on fall dataset..."
+    mu_matrix, sigma_matrix = training(fall_path_in, 1460, 1509, fall_alpha);
+    adaptive(fall_path_in, 1510, 1559, mu_matrix, sigma_matrix, fall_alpha, fall_rho);   
+    print "Computing adaptive modelling on fall dataset... done"
 
-    mu_matrix, sigma_matrix = training(traffic_path, 950, 999, traffic_alpha);
-    adaptive(traffic_path, 1000, 1049, mu_matrix, sigma_matrix, traffic_alpha, traffic_rho);
-   
+    print "Computing adaptive modelling on traffic dataset..."
+    mu_matrix, sigma_matrix = training(traffic_path_in, 950, 999, traffic_alpha);
+    adaptive(traffic_path_in, 1000, 1049, mu_matrix, sigma_matrix, traffic_alpha, traffic_rho);
+    print "Computing adaptive modelling on traffic dataset... done"
 
