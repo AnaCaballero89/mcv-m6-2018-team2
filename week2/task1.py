9d5f613f0b2ea4fbe618a29a3b6b18fd21084369
Guillem Delgado Gonzalo <guillem.delgado@gmail.com>
2018-03-05 11:24:01 +0100

    Added new metrics
    Precision and recall not working

--- week2/task1.py
+++ week2/task1.py
@@ -11,12 +11,12 @@ from gaussian import *
 from util import *
 
 # Highway sequences configuration, range 1050 - 1350
-highway_path_in = "datasets/highway/input/"	
+highway_path_in = "datasets/highway/input/"
 highway_path_gt = "datasets/highway/groundtruth/"
 
 # Fall sequences configuration, range 1460 - 1560
-fall_path_in = "datasets/fall/input/"  
-fall_path_gt = "datasets/fall/groundtruth/"  
+fall_path_in = "datasets/fall/input/"
+fall_path_gt = "datasets/fall/groundtruth/"
 
 # Traffic sequences configuration, range 950 - 1050
 traffic_path_in = "datasets/traffic/input/"
@@ -44,43 +44,53 @@ if __name__ == "__main__":
     # Initialize vectors
     vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1 = init_vectors()
    
-    print "Starting gaussian modelling on highway dataset computation..."
+    print("Starting gaussian modelling on highway dataset computation...")
     for alpha in alphas:
         mean_matrix, std_matrix = training(highway_path_in, 1050, 1199, alpha);
         AccFP, AccFN, AccTP, AccTN, AccP, AccR, AccF1 = gaussian(highway_path_in, highway_path_gt, 1200, 1349, mean_matrix, std_matrix, alpha);  
         vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1 = accumulate_values(vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1, AccFP, AccFN, AccTP, AccTN, AccP, AccR, AccF1)
-        print "Computed gaussian modelling on highway dataset with alpha "+str(alpha)
-    print "Starting gaussian modelling on highway dataset computation... done\n"
+        print("Computed gaussian modelling on highway dataset with alpha "+str(alpha))
+    print("Starting gaussian modelling on highway dataset computation... done\n")
 
     # Plot metrics on graph
     plot_graph_FP_FN_TP_TN(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'highway')
+    plot_f1_threshold(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'highway')
+    plot_precision_recall_f1(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'highway')
+    plot_precision_recall(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'highway')
 
     # Initialize vectors
     vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1 = init_vectors()
 
-    print "Starting gaussian modelling on fall dataset computation..."
+    print("Starting gaussian modelling on fall dataset computation...")
     for alpha in alphas:
         mean_matrix, std_matrix = training(fall_path_in, 1460, 1509, alpha);
         AccFP, AccFN, AccTP, AccTN, AccP, AccR, AccF1 = gaussian(fall_path_in, fall_path_gt, 1510, 1559, mean_matrix, std_matrix, alpha);  
         vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1 = accumulate_values(vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1, AccFP, AccFN, AccTP, AccTN, AccP, AccR, AccF1)
-        print "Computed gaussian modelling on fall dataset with alpha "+str(alpha)
-    print "Starting gaussian modelling on fall dataset computation... done\n"
+        print("Computed gaussian modelling on fall dataset with alpha "+str(alpha))
+    print("Starting gaussian modelling on fall dataset computation... done\n")
 
     # Plot metrics on graph
     plot_graph_FP_FN_TP_TN(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'fall')
+    plot_f1_threshold(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'fall')
+    plot_precision_recall_f1(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'fall')
+    plot_precision_recall(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'fall')
+
 
     # Initialize vectors
     vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1 = init_vectors()
 
-    print "Starting gaussian modelling on traffic dataset computation..."
+    print("Starting gaussian modelling on traffic dataset computation...")
     for alpha in alphas:
         mean_matrix, std_matrix = training(traffic_path_in, 950, 999, alpha);
         AccFP, AccFN, AccTP, AccTN, AccP, AccR, AccF1 = gaussian(traffic_path_in, traffic_path_gt, 1000, 1049, mean_matrix, std_matrix, alpha); 
         vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1 = accumulate_values(vec_FP, vec_FN, vec_TP, vec_TN, vec_P, vec_R, vec_F1, AccFP, AccFN, AccTP, AccTN, AccP, AccR, AccF1)
-        print "Computed gaussian modelling on traffic with alpha "+str(alpha)
-    print "Starting gaussian modelling on traffic dataset computation... done\n"
+        print("Computed gaussian modelling on traffic with alpha "+str(alpha))
+    print("Starting gaussian modelling on traffic dataset computation... done\n")
 
     # Plot metrics on graph
     plot_graph_FP_FN_TP_TN(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'traffic')
+    plot_f1_threshold(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'traffic')
+    plot_precision_recall_f1(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'traffic')
+    plot_precision_recall(vec_FP, vec_FN, vec_TP, vec_TN, alphas, 'traffic')
 
 
--- week2/util.py
+++ week2/util.py
@@ -32,6 +32,74 @@ def plot_graph_FP_FN_TP_TN(FP, FN, TP, TN, alphas, name):
     plt.legend(handles=[red_patch, blue_patch, green_patch, orange_patch])
     plt.show()
 
+def plot_f1_threshold(FP, FN, TP, TN, alphas, name):
+    """
+    Description: plot f1 vs threshold
+    Input: FP, FN, TP, TN, alphas, name
+    Output: None
+    """
+    fscore = []
+    for i in range(len(alphas)):
+        precision = TP[i] / float(TP[i] + FP[i])
+        recall = TP[i] / float(TP[i] + FN[i])
+        fscore.append(2 * precision * recall / (precision + recall))
+
+    plt.title('F1 vs threshold metric '+name+' dataset')
+    plt.xlabel('Threshold')
+    plt.ylabel('F1')
+    plt.ylim([0, 1])
+    plt.xlim([0, max(alphas)])
+    plt.plot(fscore)
+    plt.show()
+
+
+
+
+def plot_precision_recall_f1(FP, FN, TP, TN, alphas, name):
+    fscore = []
+    prec = []
+    rec = []
+    for i in range(len(alphas)):
+        precision = TP[i] / float(TP[i] + FP[i])
+        recall = TP[i] / float(TP[i] + FN[i])
+        prec.append(precision)
+        rec.append(recall)
+        fscore.append(2 * precision * recall / (precision + recall))
+
+    plt.title('Precision, Recall and F1 metric '+name+' dataset')
+    plt.xlabel('Threshold')
+    plt.ylabel('Metric')
+    plt.ylim([0, 1])
+    plt.xlim([0, max(alphas)])
+    plt.plot(prec)
+    plt.plot(rec)
+    plt.plot(fscore)
+    red_patch = mpatches.Patch(color='orange', label='Precision')
+    blue_patch = mpatches.Patch(color='blue', label='Recall')
+    green_patch = mpatches.Patch(color='green', label='F1')
+    plt.legend(handles=[red_patch, blue_patch, green_patch])
+    plt.show()
+
+def plot_precision_recall(FP, FN, TP, TN, alphas, name):
+    prec = []
+    rec = []
+    for i in range(len(alphas)):
+        precision = TP[i] / float(TP[i] + FP[i])
+        recall = TP[i] / float(TP[i] + FN[i])
+        prec.append(precision)
+        rec.append(recall)
+    print("Precision:")
+    print(prec)
+    print("Recall")
+    print(rec)
+
+    plt.title('Precision and Recall metric ' + name + ' dataset')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0, 1])
+    plt.xlim([0, 1])
+    plt.plot(rec, prec)
+    plt.show()
 
 def init_vectors():
 
