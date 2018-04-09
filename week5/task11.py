869d0546b5bb20f12aff91808202f45c1b736a66
Jordi-Gene-Mola <32513931+Jordi-Gene-Mola@users.noreply.github.com>
2018-04-09 20:07:59 +0200

    Update task11.py

--- week5/task11.py
+++ week5/task11.py
@@ -7,20 +7,15 @@ import os
 import numpy as np
 from util import *
 from sort import *
+from gaussian_back_sub import *
+from train_color import *
 from scipy import ndimage
 import os.path
 import numpy as np
 
+dataset="own2" # "highway","fall", "traffic", "traffic_stabilized", "own1", "own1_stabilized", "own2", "own2_stabilized"
 # Path configurations
 video_path = "./videos/"
-# Connectivity 8 to hole filling
-structuring_element = [[1,1,1],[1,1,1],[1,1,1]]
-# Set area pixels to filter blobs
-minAreaPixels = 100
-# Set kernel to apply morphology
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-# Gaussian Mixture-based Background/Foreground Segmentation Algorithm
-fgbg = cv2.createBackgroundSubtractorMOG2()
 # Initialize centroids
 centroids = []
 # Create instance of the SORT tracker (Simple Online and Realtime Tracking)
@@ -28,6 +23,7 @@ tracker_motion = Sort()
 # Frame counter
 frame_counter = 0
 
+path_in, first_train_frame, last_train_frame, first_test_frame, last_test_frame, im_size, alpha, colorSpace, connectivity, areaPixels, ac_morphology, SE1size, SE2size = setup(dataset)
 
 if __name__ == "__main__":
 
@@ -35,57 +31,46 @@ if __name__ == "__main__":
     # Use Kalman filter to track each vehicle appearing in the sequence
     # Apply the background substraction work previously done
 
-    # Choose betwen: highway, traffic or detrac
-    path_test, first_frame, last_frame = setup("detrac")
+    mu_matrix, sigma_matrix = training_color(path_in, first_train_frame, last_train_frame, alpha, colorSpace)
 
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(video_path+"kalman_filter_"+str(path_test.split("/")[2])+".avi", fourcc, 60, (get_accumulator(path_test).shape[1], get_accumulator(path_test).shape[0]))
+    out = cv2.VideoWriter(video_path+"kalman_filter_"+dataset+".avi", fourcc, 10, (im_size[1],im_size[0]))
 
+    filenames=sorted(os.listdir(path_in))
     # Read sequence of images sorted
-    for filename in sorted(os.listdir(path_test)):
+    for filename in filenames[first_test_frame:last_test_frame]:
        
         # Display current frame
         print "Processing frame: "+str(filename)
 
-        # Check that frame is into range
-        frame_num = int(filename[3:8])
-        if frame_num >= first_frame and frame_num <= last_frame:
+        # Read image from groundtruth
+        frame = cv2.imread(path_in+filename)
 
-            # Read image from groundtruth 
-            frame = cv2.imread(path_test+filename)
+        # Apply background subraction
+        background_filtered = gaussian_color(frame, mu_matrix, sigma_matrix, alpha, colorSpace, connectivity, areaPixels,ac_morphology,SE1size,SE2size)
+        cv2.imwrite("bk_subs_images/"+filename[0:(len(filename)-4)]+".jpg",np.uint8(background_filtered))
 
-            # Apply background subraction
-            background = fgbg.apply(frame)
-            background = cv2.morphologyEx(background, cv2.MORPH_OPEN, kernel)
-           
-            # Filter detections by area
-            background_filtered = area_filtering(background, minAreaPixels)
+        # Track blobs
+        centroids = get_centroids(background_filtered, areaPixels)
 
-            # Apply hole filling
-            background_filtered = ndimage.binary_fill_holes(background_filtered, structure=structuring_element).astype(float)
-            background_filtered = np.float32(background_filtered)
+        # Update tracker motion
+        dets = np.array(centroids)
+        trackers = tracker_motion.update(dets)
 
-            # Track blobs
-            centroids = get_centroids(background_filtered, minAreaPixels)
+        # Save tracker values
+        save_tracker_positions(trackers)
 
-            # Update tracker motion
-            dets = np.array(centroids)
-            trackers = tracker_motion.update(dets)
-
-            # Save tracker values
-            save_tracker_positions(trackers)
-
-            # Show results
-            frame = display_detections(frame, background_filtered, minAreaPixels)
-            frame = display_motion(frame, trackers)
-            cv2.imshow("tracking KALMAN FILTER", frame)
-            cv2.waitKey(1)
+        # Show results
+        frame = display_detections(frame, background_filtered, areaPixels)
+        frame = display_motion(frame, trackers)
+        cv2.imshow("tracking KALMAN FILTER", frame)
+        cv2.waitKey(1)
  
-            # Write frame into video
-            out.write(np.uint8(frame))
+        # Write frame into video
+        out.write(np.uint8(frame))
 
-            # Update frame_counter
-            frame_counter = frame_counter + 1
+        # Update frame_counter
+        frame_counter = frame_counter + 1
 
          
